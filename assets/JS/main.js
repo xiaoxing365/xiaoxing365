7fd13f828ef9c7297d29a4b213847f924d80e49c
xiaoxing365 <mcxiaoxing365@163.com>
2025-09-16 21:22:55 +0800

    改变github为bing网站获取

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -27,24 +27,6 @@ var iUp = (function () {
 	};
 })();
 
-function getBingImages(imgUrls) {
-	/**
-	 * 获取Bing壁纸
-	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
-	 * 然后读取 images.json 文件中的数据
-	 */
-	var indexName = "bing-image-index";
-	var index = sessionStorage.getItem(indexName);
-	var panel = document.querySelector('#panel');
-	if (isNaN(index) || index == 7) index = 0;
-	else index++;
-	var imgUrl = imgUrls[index];
-	var url = "https://www.cn.bing.com" + imgUrl;
-	panel.style.background = "url('https://bing.img.run/rand_uhd.php') center center no-repeat #666";
-	panel.style.backgroundSize = "cover";
-	sessionStorage.setItem(indexName, index);
-}
-
 function decryptEmail(encoded) {
 	var address = atob(encoded);
 	window.location.href = "mailto:" + address;
